430f43e111f65ebd069b8039875f125f7cecc061
pravalika17 <prachamalla@deloitte.com>
2016-09-10 14:04:15 +0530

    initial commits

--- app/Breadcrumb/breadcrumb.component.ts
+++ app/Breadcrumb/breadcrumb.component.ts
@@ -1,8 +1,11 @@
 import {Component} from 'angular2/core';
+import {NGL_DIRECTIVES, provideNglConfig} from 'ng-lightning/ng-lightning';
 
 @Component({
   moduleId:module.id,
   selector: 'bread-crumb',
+  directives: [NGL_DIRECTIVES],
   templateUrl: './breadcrumb.component.html'
 })
 export class Angular2Breadcrumb {}
+bootstrap(Angular2Breadcrumb, [provideNglConfig()]);
